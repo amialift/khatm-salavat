035ae013c1d0907c8390e91d94495e341d860196
amialift <m7s7a2a3a1@gmail.com>
2026-07-30 04:45:01 +0330

    Create initial Compose UI structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "com.amiralift.khatmsalavat"
+
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -15,6 +16,7 @@ android {
         applicationId = "com.amiralift.khatmsalavat"
         minSdk = 26
         targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
 
@@ -28,29 +30,51 @@ android {
             }
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
+
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.activity.compose)
+
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.compose.material.icons.extended)
+
     implementation(libs.androidx.compose.ui)
+
     implementation(libs.androidx.compose.ui.graphics)
+
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+
     testImplementation(libs.junit)
+
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
+
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(libs.androidx.junit)
+
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
---
+++ app/src/main/java/com/amiralift/khatmsalavat/ui/components/AppTopBar.kt
@@ -0,0 +1,42 @@
+package com.amiralift.khatmsalavat.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopBar(
+    title: String,
+    onMenuClick: () -> Unit
+) {
+
+    CenterAlignedTopAppBar(
+
+        title = {
+            Text(text = title)
+        },
+
+        navigationIcon = {
+
+            IconButton(
+                onClick = onMenuClick
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+
+            }
+
+        }
+
+    )
+
+}
