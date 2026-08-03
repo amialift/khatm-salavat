8b88e4aee5ab919541ef5a02c63c47462d9f8b30
amialift <m7s7a2a3a1@gmail.com>
2026-08-03 13:29:37 +0330

    Optimize drawer navigation performance

--- app/src/main/java/com/amiralift/khatmsalavat/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/amiralift/khatmsalavat/ui/screens/home/HomeScreen.kt
@@ -12,240 +12,146 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.amiralift.khatmsalavat.ui.components.AppDrawer
 import com.amiralift.khatmsalavat.ui.components.AppTopBar
 import com.amiralift.khatmsalavat.ui.components.MenuCard
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
-import androidx.compose.ui.unit.dp
-
 
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
 
-
     val drawerState = rememberDrawerState(
         initialValue = DrawerValue.Closed
     )
 
-
     val scope = rememberCoroutineScope()
 
+    fun navigateFromDrawer(route: String) {
 
+        scope.launch {
+            drawerState.close()
+        }
+
+        navController.navigate(route) {
+
+            launchSingleTop = true
+
+            restoreState = true
+
+            popUpTo("home") {
+                saveState = true
+            }
+
+        }
+    }
 
     ModalNavigationDrawer(
 
         drawerState = drawerState,
 
-
         drawerContent = {
-
 
             AppDrawer(
 
                 onQuranPeopleClick = {
 
-
-                    scope.launch {
-
-                        try {
-
-                            drawerState.close()
-
-                        } catch (_: CancellationException) {
-
-
-                        }
-
-
-                        navController.navigate(
-                            "quran_people"
-                        )
-
-                    }
-
+                    navigateFromDrawer("quran_people")
 
                 },
 
-
                 onSalavatRoundsClick = {
 
-
-                    scope.launch {
-
-                        try {
-
-                            drawerState.close()
-
-                        } catch (_: CancellationException) {
-
-
-                        }
-
-
-                        navController.navigate(
-                            "salavat_rounds"
-                        )
-
-                    }
-
+                    navigateFromDrawer("salavat_rounds")
 
                 }
-
 
             )
 
-
         }
-
-
 
     ) {
 
-
-
         Scaffold(
-
 
             topBar = {
 
-
                 AppTopBar(
-
 
                     title = "ختم صلوات",
 
-
                     onMenuClick = {
 
+                        scope.launch {
 
-                        if (!drawerState.isOpen &&
-                            !drawerState.isAnimationRunning
-                        ) {
+                            if (drawerState.currentValue == DrawerValue.Closed) {
 
-
-                            scope.launch {
-
-
-                                try {
-
-
-                                    drawerState.open()
-
-
-                                } catch (_: CancellationException) {
-
-
-                                }
-
+                                drawerState.open()
 
                             }
 
-
                         }
-
 
                     }
 
-
                 )
 
-
             }
-
-
 
         ) { innerPadding ->
 
-
-
             Column(
 
-
                 modifier = Modifier
-
                     .fillMaxSize()
-
                     .padding(innerPadding)
-
                     .padding(horizontal = 20.dp),
-
 
                 horizontalAlignment = Alignment.CenterHorizontally,
 
-
                 verticalArrangement = Arrangement.Center
-
 
             ) {
 
-
-
                 Column(
-
 
                     verticalArrangement = Arrangement.spacedBy(16.dp)
 
-
                 ) {
 
-
-
                     MenuCard(
-
 
                         title = "📖 ختم قرآن",
 
-
                         onClick = {
 
-
-                            navController.navigate(
-                                "quran"
-                            )
-
+                            navController.navigate("quran")
 
                         }
 
-
                     )
-
-
 
                     MenuCard(
 
-
                         title = "🌹 قرعه معصومین",
-
 
                         onClick = {
 
-
-                            navController.navigate(
-                                "masoom"
-                            )
-
+                            navController.navigate("masoom")
 
                         }
 
-
                     )
-
 
                 }
 
-
             }
-
-
 
         }
 
-
     }
-
 
 }
